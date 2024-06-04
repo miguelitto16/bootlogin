0b436a2db72910d9e1a1ee449c6ef2ce9965eafa
Miguel Silva <miguel.luc28@gmail.com>
2024-06-04 12:47:58 -0300

    Tradução da pág

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en" class="h-100">
+<html lang="pt" class="h-100">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -42,23 +42,23 @@
     <div>
       <h3 class="float-md-start mb-0">Cover</h3>
       <nav class="nav nav-masthead justify-content-center float-md-end">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link active" aria-current="page" href="#">Casa</a>
+        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link" href="cadastro.php">Cadastro</a>
       </nav>
     </div>
   </header>
 
   <main class="px-3">
-    <h1>Cover your page.</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <h1>Sua Página</h1>
+    <p class="lead">A capa é um modelo de uma página para construir páginas iniciais simples e bonitas. Baixe, edite o texto e adicione sua própria foto de fundo em tela cheia para torná-la sua.</p>
     <p class="lead">
-      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+      <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Saber mais</a>
     </p>
   </main>
 
   <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://github.com/miguelitto16" class="text-white">@Miguel</a>.</p>
+    <p>Feito por </a><a href="https://github.com/miguelitto16" class="text-white">@Miguel</a>.</p>
   </footer>
 </div>
 
